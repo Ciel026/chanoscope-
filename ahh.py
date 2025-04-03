6b27f5e66a4a56249d84581590f731c6fede30b5
Ciel026 <franciscovenise2@gmail.com>
2025-04-03 23:30:36 +0800

    Update ahh.py

--- ahh.py
+++ ahh.py
@@ -49,5 +49,5 @@ def predict():
     return jsonify({'prediction': result.tolist()})
 
 if __name__ == '__main__':
-    # Run the app on port 5000
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 8000))  # Default to 8000 if PORT isn't set
+    uvicorn.run(app, host="0.0.0.0", port=port)
